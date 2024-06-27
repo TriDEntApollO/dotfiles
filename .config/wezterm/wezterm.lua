e653ae7543e680b8b62c6b7392e0ff832ccf8ac2
TriDEntApollO <tridentapollo.llgic@gmail.com>
2024-06-27 19:26:01 +0530

    Changed tabs looksl

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,47 +1,308 @@
 local wezterm = require 'wezterm'
+local act = wezterm.action
+local workspace_saver = require("wezterm_workspace_saver")
+-- local session_manager = require("session-manager")
+
+wezterm.on("save_state", function(window) workspace_saver.save_state(window) end)
+wezterm.on("load_state", function() workspace_saver.load_state() end)
+wezterm.on("restore_state", function(window) workspace_saver.restore_state(window) end)
+
+-- ### Functions ### --
+
+local get_last_folder_segment = function(cwd)
+  if cwd == nil then
+    return "N/A" -- or some default value you prefer
+  end
+
+  -- Strip off 'file:///' if present
+  local pathStripped = cwd:match("^file:///(.+)") or cwd
+  -- Normalize backslashes to slashes for Windows paths
+  pathStripped = pathStripped:gsub("\\", "/")
+  -- Split the path by '/'
+  local path = {}
+  for segment in string.gmatch(pathStripped, "[^/]+") do
+    table.insert(path, segment)
+  end
+  return path[#path] -- returns the last segment
+end
+
+local function get_current_working_dir(tab)
+  local current_dir = tab.active_pane.current_working_dir or ''
+  return get_last_folder_segment(current_dir)
+end
+
+local process_icons = {
+  ['docker'] = wezterm.nerdfonts.linux_docker,
+  ['docker-compose'] = wezterm.nerdfonts.linux_docker,
+  ['psql'] = wezterm.nerdfonts.dev_postgresql,
+  ['kuberlr'] = wezterm.nerdfonts.linux_docker,
+  ['kubectl'] = wezterm.nerdfonts.linux_docker,
+  ['stern'] = wezterm.nerdfonts.linux_docker,
+  ['nvim'] = wezterm.nerdfonts.custom_vim,
+  ['make'] = wezterm.nerdfonts.seti_makefile,
+  ['vim'] = wezterm.nerdfonts.dev_vim,
+  ['go'] = wezterm.nerdfonts.seti_go,
+  ['zsh'] = wezterm.nerdfonts.dev_terminal,
+  ['bash'] = wezterm.nerdfonts.cod_terminal_bash,
+  ['btm'] = wezterm.nerdfonts.mdi_chart_donut_variant,
+  ['htop'] = wezterm.nerdfonts.mdi_chart_donut_variant,
+  ['cargo'] = wezterm.nerdfonts.dev_rust,
+  ['sudo'] = wezterm.nerdfonts.fa_hashtag,
+  ['lazydocker'] = wezterm.nerdfonts.linux_docker,
+  ['git'] = wezterm.nerdfonts.dev_git,
+  ['lua'] = wezterm.nerdfonts.seti_lua,
+  ['wget'] = wezterm.nerdfonts.mdi_arrow_down_box,
+  ['curl'] = wezterm.nerdfonts.mdi_flattr,
+  ['gh'] = wezterm.nerdfonts.dev_github_badge,
+  ['ruby'] = wezterm.nerdfonts.cod_ruby,
+  ['pwsh'] = wezterm.nerdfonts.seti_powershell,
+  ['node'] = wezterm.nerdfonts.dev_nodejs_small,
+  ['dotnet'] = wezterm.nerdfonts.md_language_csharp,
+}
+local function get_process(tab)
+  local process_name = tab.active_pane.foreground_process_name:match("([^/\\]+)%.exe$") or
+      tab.active_pane.foreground_process_name:match("([^/\\]+)$")
+
+  -- local icon = process_icons[process_name] or string.format('[%s]', process_name)
+  local icon = process_icons[process_name] or wezterm.nerdfonts.seti_checkbox_unchecked
+
+  return icon
+end
+
+local function basename(s)
+  return string.gsub(s, '(.*[/\\])(.*)', '%2')
+end
+
+-- ### Main Config ### --
+
 local config = {}
 
-config.color_scheme = 'Dracula (Official)'
+config = wezterm.config_builder()
 
-config.font_size = 14
+-- Color Scheme
+
+config.color_scheme = 'Catppuccin Macchiato'
+
+-- Font
+
+config.font_size = 12
 config.font = wezterm.font('JetBrainsMono Nerd Font', {weight = 'Bold', italic = false})
 
-config.default_prog = {'/bin/zsh'}
+-- General
 
-config.hide_tab_bar_if_only_one_tab = true
+config.default_prog = {'/bin/zsh'}
+config.scrollback_lines = 3000
 config.hide_mouse_cursor_when_typing = true
+
+-- Cursor
 
 config.default_cursor_style = 'BlinkingUnderline'
 config.cursor_thickness = '150%'
 config.cursor_blink_rate = 800
 
-config.window_background_opacity = 0.75
+-- Tab/Status Bar
 
+config.use_fancy_tab_bar = false
+config.tab_bar_at_bottom = true
+config.hide_tab_bar_if_only_one_tab = true
+config.status_update_interval = 1000
+config.tab_max_width = 60
+-- config.tab_bar_fill = true
+
+-- Panes
+
+config.inactive_pane_hsb = {
+  saturation = 0.6,
+  brightness = 0.7,
+}
+
+-- Window
+
+config.window_background_opacity = 0.75
 config.window_decorations = "TITLE | RESIZE"
+config.window_close_confirmation = 'AlwaysPrompt'
 
 config.window_padding = {
-  left = '1cell',
-  right = '1cell',
-  top = '0.5cell',
-  bottom = '0.5cell',
+--  left = '1cell',
+--  right = '1cell',
+--  top = '0.5cell',
+--  bottom = '0.5cell',
+  top = 7,
+  right = 7,
+  bottom = 2,
+  left = 7,  
 }
 
 config.window_frame = {
   font = require('wezterm').font 'Roboto',
   font_size = 12,
 
-  inactive_titlebar_bg = '#1d1236',
-  active_titlebar_bg = '#4d2d8c',
+  inactive_titlebar_bg = '#3e2569',
+  active_titlebar_bg = '#7e56c2',
   inactive_titlebar_fg = '#cccccc',
   active_titlebar_fg = '#ffffff',
-  inactive_titlebar_border_bottom = '#2b2042',
-  active_titlebar_border_bottom = '#4d2d8c',
+  inactive_titlebar_border_bottom = '#3e2569',
+  active_titlebar_border_bottom = '#7e56c2',
   button_fg = '#cccccc',
-  button_bg = '#4d2d8c',
+  button_bg = '#7e56c2',
   button_hover_fg = '#ffffff',
-  button_hover_bg = '#7c4fd6',
+  button_hover_bg = '#a57bed',
 }
 
-config.tab_max_width = 100
+wezterm.on('format-tab-title', function(tab, tabs, panes, config, hover, max_width)
+  local has_unseen_output = false
+  local is_zoomed = false
+
+  for _, pane in ipairs(tab.panes) do
+    if not tab.is_active and pane.has_unseen_output then
+      has_unseen_output = true
+    end
+    if pane.is_zoomed then
+      is_zoomed = true
+    end
+  end
+
+  local cwd = get_current_working_dir(tab)
+  local process = get_process(tab)
+  local zoom_icon = is_zoomed and wezterm.nerdfonts.cod_zoom_in or ""
+  local title = string.format(' %s ~ %s %s ', process, cwd, zoom_icon) -- Add placeholder for zoom_icon
+
+  return wezterm.format({
+    { Attribute = { Intensity = 'Bold' } },
+    { Text = title }
+  })
+end)
+
+wezterm.on("update-right-status", function(window, pane)
+  local workspace_or_leader = window:active_workspace()
+  -- Change the worspace name status if leader is active
+  if window:active_key_table() then workspace_or_leader = window:active_key_table() end
+  if window:leader_is_active() then workspace_or_leader = "LEADER" end
+
+  local cwd = pane:get_current_working_dir() or "N/A"
+  local last_folder = get_last_folder_segment(cwd)
+  local cmd = get_last_folder_segment(pane:get_foreground_process_name())
+  local time = wezterm.strftime("%H:%M")
+  local hostname = " " .. wezterm.hostname() .. " ";
+
+  window:set_right_status(wezterm.format({
+    { Text = wezterm.nerdfonts.oct_table .. " " .. workspace_or_leader },
+    { Text = " | " },
+    -- { Text = wezterm.nerdfonts.md_folder .. " " .. last_folder },
+    -- { Text = " | " },
+    { Foreground = { Color = "FFB86C" } },
+    { Text = wezterm.nerdfonts.fa_code .. " " .. cmd },
+    "ResetAttributes",
+    { Text = " | " },
+    { Text = wezterm.nerdfonts.oct_person .. " " .. hostname },
+    { Text = " | " },
+    { Text = wezterm.nerdfonts.md_clock .. " " .. time },
+    { Text = " | " },
+  }))
+end)
+
+-- Skip close confirmation for  
+
+config.skip_close_confirmation_for_processes_named = {
+  'bash',
+  'sh',
+  'zsh',
+  'fish',
+  'tmux',
+  'nu',
+  'cmd.exe',
+  'pwsh.exe',
+  'powershell.exe',
+}
+
+-- Keys
+
+config.leader = { key = "Space", mods = "SHIFT", timeout_milliseconds = 3000 }
+config.keys = {
+  { key = "c", mods = "LEADER", action = act.ActivateCopyMode },
+
+  -- Pane Keybindings
+  { key = "-", mods = "LEADER", action = act.SplitVertical { domain = "CurrentPaneDomain" } },
+  { key = "|", mods = "LEADER", action = act.SplitHorizontal { domain = "CurrentPaneDomain" } },
+  { key = "h", mods = "LEADER", action = act.ActivatePaneDirection("Left") },
+  { key = "j", mods = "LEADER", action = act.ActivatePaneDirection("Down") },
+  { key = "k", mods = "LEADER", action = act.ActivatePaneDirection("Up") },
+  { key = "l", mods = "LEADER", action = act.ActivatePaneDirection("Right") },
+  { key = "x", mods = "LEADER", action = act.CloseCurrentPane { confirm = true } },
+  { key = "z", mods = "LEADER", action = act.TogglePaneZoomState },
+  { key = "s", mods = "LEADER", action = act.RotatePanes "Clockwise" },
+  -- We could make separate keybindings for resizing panes
+  -- But Wezterm offers a custom mode we will use here
+  { key = "r", mods = "LEADER", action = act.ActivateKeyTable { name = "resize_pane", one_shot = false } },
+
+  -- Tab Keybindings
+  { key = "c", mods = "LEADER", action = act.SpawnTab("CurrentPaneDomain") },
+  { key = "n", mods = "LEADER", action = act.ActivateTabRelative(1) },
+  { key = "p", mods = "LEADER", action = act.ActivateTabRelative(-1) },
+  { key = "t", mods = "LEADER", action = act.ShowTabNavigator },
+  -- Table for moving tabs around
+  { key = "m", mods = "LEADER", action = act.ActivateKeyTable { name = "move_tab", one_shot = false } },
+
+  -- Workspace
+  { key = "w", mods = "LEADER", action = act.ShowLauncherArgs { flags = "FUZZY|WORKSPACES" } },
+  {
+    key = 'W',
+    mods = 'LEADER',
+    action = act.PromptInputLine {
+      description = wezterm.format {
+        { Attribute = { Intensity = 'Bold' } },
+        { Foreground = { AnsiColor = 'Fuchsia' } },
+        { Text = 'Enter name for new workspace' },
+      },
+      action = wezterm.action_callback(function(window, pane, line)
+        -- line will be `nil` if they hit escape without entering anything
+        -- An empty string if they just hit enter
+        -- Or the actual line of text they wrote
+        if line then
+          window:perform_action(
+            act.SwitchToWorkspace {
+              name = line,
+            },
+            pane
+          )
+        end
+      end),
+    },
+  },
+
+  -- Experimental section for workspace saving
+  { key = "S", mods = "LEADER", action = wezterm.action({ EmitEvent = "save_state" }) },
+  { key = "R", mods = "LEADER", action = wezterm.action({ EmitEvent = "restore_state" }) },
+  { key = "L", mods = "LEADER", action = wezterm.action({ EmitEvent = "load_state" }) },
+}
+
+-- Quick tab movement
+
+for i = 1, 9 do
+  table.insert(config.keys, {
+    key = tostring(i),
+    mods = "LEADER",
+    action = act.ActivateTab(i - 1)
+  })
+end
+
+config.key_tables = {
+  resize_pane = {
+    { key = "h",      action = act.AdjustPaneSize { "Left", 1 } },
+    { key = "j",      action = act.AdjustPaneSize { "Down", 1 } },
+    { key = "k",      action = act.AdjustPaneSize { "Up", 1 } },
+    { key = "l",      action = act.AdjustPaneSize { "Right", 1 } },
+    { key = "Escape", action = "PopKeyTable" },
+    { key = "Enter",  action = "PopKeyTable" },
+  },
+  move_tab = {
+    { key = "h",      action = act.MoveTabRelative(-1) },
+    { key = "j",      action = act.MoveTabRelative(-1) },
+    { key = "k",      action = act.MoveTabRelative(1) },
+    { key = "l",      action = act.MoveTabRelative(1) },
+    { key = "Escape", action = "PopKeyTable" },
+    { key = "Enter",  action = "PopKeyTable" },
+  }
+}
 
 return config
